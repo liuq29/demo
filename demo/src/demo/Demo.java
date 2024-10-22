2f9f5399f17d586e295d7da47354b47be89fa9fd
liuq <1453823524@qq.com>
2024-10-22 21:41:10 +0800

    Update Demo.java

--- demo/src/demo/Demo.java
+++ demo/src/demo/Demo.java
@@ -1,5 +1,7 @@
-private final JavaClasses classes = new ClassFileImporter()
-            .importPackages("com.yourproject")
-            .stream()
+// 扫描项目包中的类
+    private final JavaClasses allClasses = new ClassFileImporter().importPackages("com.yourproject");
+
+    // 过滤后的类，排除com.cn.package下的所有类
+    private final Set<JavaClass> classes = allClasses.stream()
             .filter(clazz -> !clazz.getName().startsWith("com.cn.package")) // 排除特定包
             .collect(Collectors.toSet());
